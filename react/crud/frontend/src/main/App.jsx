ef74670a906ad69cc02c70c60c68b420153e66e0
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2024-08-26 20:10:30 -0300

    .

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -3,16 +3,19 @@ import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import React from 'react';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import Logo from '../components/template/Logo';
 import Nav from '../components/template/Nav';
-import Main from '../components/template/Main';
+import Routes from './Routes'
 import Footer from '../components/template/Footer';
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Main icon='home' tittle="Início" subtitle="Segundo Projeto do capítulo de React." />
-        <Footer />
-    </div>
-
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div >
+    </BrowserRouter>
