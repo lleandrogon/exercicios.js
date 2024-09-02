80def00a1427e7d807c61d78626fb9d50d9fb3da
Leandro Gonçalves <lleandrogon2004@gmail.com>
2024-09-01 21:48:06 -0300

    .

--- next-crud/.next/static/chunks/webpack.js
+++ next-crud/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6aa8c93f93f3fd34"; }
+/******/ 		__webpack_require__.h = function() { return "30a4d20110b4683f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,8 +1,26 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../../core/Cliente";
 
 export default function Home() {
+
+    const clientes = [
+        new Cliente('Ana', 34, '1'),
+        new Cliente('Bia', 45, '2'),
+        new Cliente('Carlos', 23, '3'),
+        new Cliente('Pedro', 54, '4'),
+    ]
+
+    function clienteSelecionado(cliente: Cliente) {
+        console.log(cliente.nome)
+    }
+
+    function clienteExcluido(cliente: Cliente) {
+        console.log(`Excluir... ${cliente.nome}`)
+    }
+
     return (
         <div className={`
             flex justify-center items-center h-screen
@@ -10,7 +28,9 @@ export default function Home() {
             text-white
         `}>
             <Layout titulo="Cadastro Simples">
-                <span>Conteudo</span>
+                <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado}
+                clienteExcluido={clienteExcluido}
+            />
             </Layout>
         </div>
     )
