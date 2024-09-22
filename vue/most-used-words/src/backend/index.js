bffa23116648321215bfe3e04101654de8a4436d
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2024-09-21 21:14:22 -0300

    .

---
+++ vue/most-used-words/src/backend/index.js
@@ -0,0 +1,16 @@
+const { ipcMain } = require("electron")
+
+const pathToRows = require("./pathsToRows")
+
+ipcMain.on("process-subtitles", (event, paths) => {
+    console.log("paths")
+    pathToRows(paths)
+        .then(rows => console.log(rows))
+        .then(() => {
+            event.reply("process-subtitles", [
+                { word: "i", amount: 547 },
+                { word: "you", amount: 478 },
+                { word: "it", amount: 10 }
+            ])
+        })
+})
--- vue/most-used-words/src/components/HomePage.vue
+++ vue/most-used-words/src/components/HomePage.vue
@@ -1,23 +1,46 @@
 <template>
-    <div class="pills">
-        <PillComponent v-for="item in groupedWords" :word="item.word"
-        :amount="item.amount" :key="item.word"/>
+    <div class="home">
+        <v-form>
+            <v-file-input
+            label="Selecione as legendas"
+            prepend-icon="mdi-messasge-text"
+            append-icon="mdi-send"
+            outlined
+            multiple
+            v-model="files"
+            @click:append="processSubtitles"
+            >
+            </v-file-input>
+        </v-form>
+
+        <div class="pills">
+            <PillComponent v-for="item in groupedWords" :word="item.word"
+            :amount="item.amount" :key="item.word"/>
+        </div>
     </div>
 </template>
 
 <script>
 import PillComponent from './PillComponent.vue';
+import { ipcRenderer } from 'electron';
 export default {
     components: {
         PillComponent
     },
     data: function (){
         return {
-            groupedWords:[
-                {word: "i", amount:547},
-                {word:"you", amount: 478},
-                {word: "it", amount: 10}
-            ]
+            files:[],
+            groupedWords:[]
+        }
+    },
+    methods: {
+        processSubtitles(){
+            let paths = this.files.map(f=>f.path)
+            ipcRenderer.send("process-subtitles", paths)
+            ipcRenderer.on("process-subtitles", (event, resp)=>{
+                this.groupedWords = resp
+            })
+            
         }
     }
 }
@@ -25,8 +48,14 @@ export default {
 
 <style scoped>
 
+.home {
+    margin: 20px;
+}
+
 .pills {
     display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 }
 
 </style>
